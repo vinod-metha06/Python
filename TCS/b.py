efc55c9f77fe876c99ac1453eaa4d042476f9c3c
vinod-metha06 <methavinod06@gmail.com>
2021-10-08 14:02:55 +0530

    updated..

--- TCS/b.py
+++ TCS/b.py
@@ -6,23 +6,38 @@ k=int(input())
 b=[]
 p=0
 j=0
+
+c=[]
+d=[]
+f=[]
 for i in range(l):
-    if i !=k and i>k:
-        if j!=0:
-            b.append(a[k])
-            
-            k+=1
-    elif i!=k and i<k:
-        if j==0:
-            b.append(a[i])
-        
-    elif i==k:
-        j=1
-        if p==0:
-            b.append(a[-1])
-            p+=1
-         
+    if i==k:
+       d=a[k:l]
+    else:
+       f=a[0:k]
+d.extend(f)
     
-print(b)
+
+
+
+# for i in range(l):
+#     if i !=k and i>k:
+#         if j!=0:
+#             b.append(a[k])
+            
+#             k+=1
+#     elif i!=k and i<k:
+#         if j==0:
+#             b.append(a[i])
+        
+#     elif i==k:
+#         j=1
+#         if p==0:
+#             b.append(a[-1])
+#             p+=1
+         
+for i in range(l):
+    
+   print(d[i],end=" ")
         
     
