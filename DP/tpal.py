97b5845d876a6aa5b1127fd1141291ebd9752eea
vinod-metha06 <methavinod06@gmail.com>
2022-03-21 16:00:33 +0530

    21.03.22

--- DP/tpal.py
+++ DP/tpal.py
@@ -1,3 +1,6 @@
+
+
+
 s=input()
 a=[]
 k=-1
@@ -16,13 +19,14 @@ if "" in l:
 r=list(l)
 sorted_list = list(sorted(r, key = len))
 
-print(sorted_list)
+#print(sorted_list)
 
 def getapal(sorted_list):
-    for i in range(0,len(sorted_list)):
-        t=sorted_list[i]
+    for i in range(1,len(sorted_list)+1):
+        t=sorted_list[-i]
         if t==t[::-1]:
-            print(sorted_list[i])
+            print(t)
+            break
 
 getapal(sorted_list)
         
